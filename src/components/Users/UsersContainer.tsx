da07015a64b631dc6a1efbcb7f95457d249b3a4e
achernenkov <jardagorng@gmail.com>
2020-12-23 18:01:45 +0300

    Add container API components.

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -2,12 +2,31 @@ import React from "react";
 import {connect} from "react-redux";
 import Users from "./Users";
 import {dispatchType, RootStateType} from "../../redux/redux-store";
-import {followAC,unFollowAC, UsersStateType} from "../../redux/users-reducer";
+import {followAC, unFollowAC, UsersStateType} from "../../redux/users-reducer";
 import axios from "axios";
+import s from "./Users.module.css";
 
 axios.get('https://social-network.samuraijs.com/api/1.0/users').then(obj => {
+    debugger
     console.log(obj.data.items)
 })
+
+type UsersPropsType = {
+    followAC: (userID: number) => void
+    state: Array<UsersStateType>
+    unFollowAC: (userID: number) => void
+}
+
+class UsersAPIContainer extends React.Component<UsersPropsType> {
+    render() {
+        return (
+            <Users
+                followAC={this.props.followAC}
+                unFollowAC={this.props.unFollowAC}
+                state={this.props.state}
+            />)
+    }
+}
 
 
 type MSTPType = {
@@ -39,6 +58,6 @@ let mapDispatchToProps = (dispatch: dispatchType): MDTPType => {
 }
 
 
-let UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+let UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIContainer)
 
 export default UsersContainer
