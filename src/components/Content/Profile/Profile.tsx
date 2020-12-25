73762fedb2f9a588165188242ff4471060957743
achernenkov <jardagorng@gmail.com>
2020-12-25 23:11:26 +0300

    Add profile info.

--- src/components/Content/Profile/Profile.tsx
+++ src/components/Content/Profile/Profile.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {UserProfileType} from "../../../redux/userProfile-reducer";
+import s from './../Content.module.css'
 
 
 
 const Profile = (props: UserProfileType) => {
     return (
         <div>
-            <img src='https://wallpaperaccess.com/full/159449.jpg'/>
+            <img src='https://wallpaperaccess.com/full/159449.jpg' className={s.imgBG}/>
             <div>
-                {props.fullName}
-                аватар + описание
+                <img src={props.photos.large} width='200px' height='200px'/>
+                <div>Полное имя: {props.fullName}</div>
+                <div>Поиска работы: {props.lookingForAJobDescription}</div>
             </div>
         </div>
     )
