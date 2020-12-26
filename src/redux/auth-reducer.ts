a9b25e4fae9e41b376c621af81797db1023ec202
achernenkov <jardagorng@gmail.com>
2020-12-26 18:35:01 +0300

    Created auth reducer.

---
+++ src/redux/auth-reducer.ts
@@ -0,0 +1,35 @@
+export type AuthStateType = {
+    id: number | null
+    login: string | null
+    email: string | null
+}
+
+let initialState = {
+    id: null,
+    login: null,
+    email: null,
+}
+
+export type SetDataToAuthStateType = {
+    type: 'SET-DATA-AUTH-STATE'
+    payload: {
+        id: number
+        login: string
+        email: string
+    }
+}
+
+export type AuthActionTotalType = SetDataToAuthStateType
+
+export let setDataToAuthState = (id: number, login: string, email: string): SetDataToAuthStateType => {
+    return {type: "SET-DATA-AUTH-STATE", payload: {id, login, email}}
+}
+
+export let authReducer = (state:AuthStateType = initialState, action: AuthActionTotalType ) => {
+    switch (action.type){
+        case "SET-DATA-AUTH-STATE":
+            return {...action.payload}
+        default:
+            return {state}
+    }
+}
